67a4505c1f4683ddefa6842da2ca034a1810d58a
Darsh Savani <savanidarsh@gmail.com>
2026-06-15 13:46:35 -0400

    Phase 2: hardened S3 logs bucket + docs

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,34 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "secure-genai-gateway-logs-darsh-1522"
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
